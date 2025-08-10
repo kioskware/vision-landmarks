818f2d679366c88f2a2f9aadd481b1f5c2ca64b3
FivesoftCode <jan.rozenbajgier@gmail.com>
2025-08-10 12:27:03 +0200

    Migrated to new vision version 1.0.3-alpha

--- landmarks/src/main/java/kioskware/vision/landmarks/Util.kt
+++ landmarks/src/main/java/kioskware/vision/landmarks/Util.kt
@@ -3,38 +3,37 @@ package kioskware.vision.landmarks
 import kioskware.vision.landmarks.`object`.Object
 import kioskware.vision.landmarks.`object`.ObjectParam
 
-fun List<Landmark>?.getByType(typeId: String): List<Landmark> {
-    return this?.filter { it.typeId == typeId } ?: emptyList()
-}
-
-fun List<Landmark>?.getByTypePrefix(typeIdPrefix: String): List<Landmark> {
-    return this?.filter { it.typeId.startsWith(typeIdPrefix) } ?: emptyList()
-}
-
-fun List<NormalizedLandmark>?.getByType(typeId: String): List<NormalizedLandmark> {
-    return this?.filter { it.typeId == typeId } ?: emptyList()
-}
-
-fun List<NormalizedLandmark>?.getByTypePrefix(typeIdPrefix: String): List<NormalizedLandmark> {
-    return this?.filter { it.typeId.startsWith(typeIdPrefix) } ?: emptyList()
-}
-
-fun List<Object>.getByType(typeId: String): List<Object> {
+@JvmName("getLandmarkByType")
+fun <T : Landmark> List<T>.getByType(typeId: String): List<T> {
     return this.filter { it.typeId == typeId }
 }
 
-fun List<Object>.getByTypePrefix(typeIdPrefix: String): List<Object> {
+@JvmName("getLandmarkByTypePrefix")
+fun <T : Landmark> List<T>.getByTypePrefix(typeIdPrefix: String): List<T> {
     return this.filter { it.typeId.startsWith(typeIdPrefix) }
 }
 
-fun List<Object>.getByTrackingId(trackingId: String): Object? {
+@JvmName("getLandmarkByTrackingId")
+fun <T : Object> List<T>.getByType(typeId: String): List<T> {
+    return this.filter { it.typeId == typeId }
+}
+
+@JvmName("getObjectByTypePrefix")
+fun <T : Object> List<T>.getByTypePrefix(typeIdPrefix: String): List<T> {
+    return this.filter { it.typeId.startsWith(typeIdPrefix) }
+}
+
+@JvmName("getObjectByTrackingId")
+fun <T : Object> List<T>.getByTrackingId(trackingId: String): T? {
     return this.firstOrNull { it.trackingId == trackingId }
 }
 
+@JvmName("getObjectParamByType")
 fun List<ObjectParam<*>>.getByType(typeId: String): ObjectParam<*>? {
     return this.firstOrNull { it.typeId == typeId }
 }
 
+@JvmName("getObjectParamByTypePrefix")
 fun List<ObjectParam<*>>.getByTypePrefix(typeIdPrefix: String): List<ObjectParam<*>> {
     return this.filter { it.typeId.startsWith(typeIdPrefix) }
 }
